be2dc4f6da783163f81019605ebb278dc2e9d190
TanqDev88 <oliverprim783@gmail.com>
2025-04-20 04:21:05 -0300

    Direccion and Person models is added

---
+++ src/main/java/com/example/person_service_api/models/Persona.java
@@ -0,0 +1,77 @@
+package com.example.person_service_api.models;
+
+public class Persona {
+    private  long id;
+    private String nombre;
+    private String apellido;
+    private Direccion direccion;
+    private String telefono;
+    private int edad;
+    private String email;
+
+    public Persona(long id, String nombre, String apellido, Direccion direccion, String telefono, String email, int edad) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.direccion = direccion;
+        this.telefono = telefono;
+        this.email = email;
+        this.edad = edad;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Direccion getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(Direccion direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+}
